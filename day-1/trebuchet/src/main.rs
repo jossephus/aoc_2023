53ddc7dec55285e48e1c66797ec0e5db3ea7993d
jossephus <yosephtuemay64@gmail.com>
2023-12-01 11:53:13 +0300

    day-1: challange 2: No it should not be such hectic

--- day-1/trebuchet/src/main.rs
+++ day-1/trebuchet/src/main.rs
@@ -1,42 +1,52 @@
-use std::{collections::HashMap, fs};
+use std::{collections::HashMap, fs, ops::Index};
 
 fn main() {
-    /*
-     *
-     *
-     * */
-    //let strings = vec![
-    //"one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
-    //];
-
-    //let digits = 1..9;
-
-    //let mut strings_to_digits = strings.iter().zip(digits);
-
-    //let mut is_lettered_digit = |i: &str| strings_to_digits.find(|(&x, y)| x == i);
-
-    //if let Some(x) = is_lettered_digit("four") {
-    //println!("{:?}", x.1);
-    //}
-
     let data = fs::read_to_string("data/data.txt").unwrap();
 
-    let splitted = data.split("\n").collect::<Vec<_>>();
+    let numbers_to_strings = HashMap::from([
+        ("one", '1'),
+        ("two", '2'),
+        ("three", '3'),
+        ("four", '4'),
+        ("five", '5'),
+        ("six", '6'),
+        ("seven", '7'),
+        ("eight", '8'),
+        ("nine", '9'),
+    ]);
+
+    let splitted = data.trim().split("\n").collect::<Vec<_>>();
 
     let mut total = 0;
 
     for i in splitted {
         let mut sum: Vec<char> = vec![];
 
-        let numbers: Vec<char> = i.chars().filter(|x| x.is_numeric()).collect();
-        println!("{:?}", numbers);
+        let mut matches: Vec<(usize, char)> = vec![];
 
-        if let Some(&first) = numbers.first() {
-            sum.push(first);
+        for number in numbers_to_strings.keys() {
+            if let Some(index) = i.find(number) {
+                matches.push((index, numbers_to_strings.get(number).unwrap().to_owned()));
+            }
         }
 
-        if let Some(&last) = numbers.last() {
-            sum.push(last);
+        let numbers: Vec<(usize, char)> = i
+            .chars()
+            .enumerate()
+            .filter(|(index, x)| x.is_numeric())
+            .map(|(x, c)| (x, c))
+            .collect();
+
+        matches.extend(numbers);
+
+        matches.sort();
+
+        if let Some(&first) = matches.first() {
+            sum.push(first.1);
+        }
+
+        if let Some(&last) = matches.last() {
+            sum.push(last.1);
         }
 
         total += sum.iter().collect::<String>().parse::<i32>().unwrap_or(0);
