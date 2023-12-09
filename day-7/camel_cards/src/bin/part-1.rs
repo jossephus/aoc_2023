9bb1a6d2e55fb5bd6144376886ff6a97f0bd176a
jossephus <yosephtuemay64@gmail.com>
2023-12-09 23:41:38 +0300

    took me a while but I figured it out

--- day-7/camel_cards/src/bin/part-1.rs
+++ day-7/camel_cards/src/bin/part-1.rs
@@ -1,30 +1,33 @@
 use itertools::Itertools;
-use std::cmp::Ordering;
+use std::{
+    cmp::Ordering,
+    collections::{HashMap, HashSet},
+};
 
 use anyhow::{anyhow, Result};
-use nom::bytes::complete::{tag, take_while1};
 use nom::{
+    bytes::complete::{tag, take, take_while1, take_while_m_n},
     character::is_digit,
     combinator::{all_consuming, map, map_res},
-    sequence::tuple,
+    sequence::{preceded, tuple},
     AsChar, Finish, IResult,
 };
 
 #[derive(Debug, PartialEq, PartialOrd, Hash, Eq, Ord, Clone, Copy)]
 enum Card {
-    J = 3,
-    H2,
-    H3,
-    H4,
-    H5,
-    H6,
-    H7,
-    H8,
-    H9,
-    T,
-    Q,
-    K,
     A,
+    K,
+    Q,
+    J,
+    T,
+    H9,
+    H8,
+    H7,
+    H6,
+    H5,
+    H4,
+    H3,
+    H2,
 }
 
 impl TryFrom<char> for Card {
@@ -52,13 +55,14 @@ impl TryFrom<char> for Card {
 
 #[derive(Debug, PartialEq, PartialOrd, Clone, Copy, Eq, Ord)]
 enum HandKind {
-    High = 1,    // 5 - different
-    OnePair,     // 2 - same, 3 different
-    TwoPair,     // 2 - same, 2 - same, 1 - different
-    ThreeOfKind, // 3- same, 2 - different
-    FullHouse,   // 3 - same, 2 - same
-    FourKind,    // 4 - same, 1 - different
-    FiveKind,    // 5 - same
+    FIVE_KIND,     // 5 - same
+    FOUR_KIND,     // 4 - same, 1 - different
+    FULL_HOUSE,    // 3 - same, 2 - same
+    THREE_OF_KIND, // 3- same, 2 - different
+    TWO_PAIR,      // 2 - same, 2 - same, 1 - different
+    ONE_PAIR,      // 2 - same, 3 different
+    HIGH,          // 5 - different
+    NONE,
 }
 
 impl TryFrom<Vec<Card>> for HandKind {
@@ -66,31 +70,11 @@ impl TryFrom<Vec<Card>> for HandKind {
 
     fn try_from(value: Vec<Card>) -> std::result::Result<Self, Self::Error> {
         // five_kind
-        let mut value = value;
-
-        if let Some(_) = value.iter().find(|&x| x == &Card::J) {
-            if value.iter().counts()[&Card::J] != 5 {
-                let highest = value
-                    .iter()
-                    .filter(|&&x| x != Card::J)
-                    .counts()
-                    .into_iter()
-                    .max_by(|(_, v), (_, p)| v.cmp(p))
-                    .unwrap()
-                    .0;
-
-                value = value
-                    .iter()
-                    .map(|&x| if x == Card::J { *highest } else { x })
-                    .collect_vec();
-            }
-        }
-
         match value.iter().unique().collect::<Vec<_>>()[..] {
-            [_] => Ok(HandKind::FiveKind),
+            [_] => Ok(HandKind::FIVE_KIND),
             [x, y] => match (value.iter().counts()[x], value.iter().counts()[y]) {
-                (4, 1) | (1, 4) => Ok(HandKind::FourKind),
-                (3, 2) | (2, 3) => Ok(HandKind::FullHouse),
+                (4, 1) | (1, 4) => Ok(HandKind::FOUR_KIND),
+                (3, 2) | (2, 3) => Ok(HandKind::FULL_HOUSE),
                 (a, b) => {
                     panic!("No we dont handle that {} {}", a, b)
                 }
@@ -100,14 +84,14 @@ impl TryFrom<Vec<Card>> for HandKind {
                 value.iter().counts()[y],
                 value.iter().counts()[z],
             ) {
-                (3, 1, 1) | (1, 1, 3) | (1, 3, 1) => Ok(HandKind::ThreeOfKind),
-                (2, 1, 2) | (2, 2, 1) | (1, 2, 2) => Ok(HandKind::TwoPair),
+                (3, 1, 1) | (1, 1, 3) | (1, 3, 1) => Ok(HandKind::THREE_OF_KIND),
+                (2, 1, 2) | (2, 2, 1) | (1, 2, 2) => Ok(HandKind::TWO_PAIR),
                 (a, b, c) => {
                     panic!("No we dont handle that {} {} {}", a, b, c)
                 }
             },
-            [_, _, _, _] => Ok(HandKind::OnePair),
-            [_, _, _, _, _] => Ok(HandKind::High),
+            [_, _, _, _] => Ok(HandKind::ONE_PAIR),
+            [_, _, _, _, _] => Ok(HandKind::HIGH),
             _ => {
                 panic!("We dont handle this");
             }
@@ -167,14 +151,14 @@ struct Game {
     bid: i32,
 }
 
-impl std::fmt::Display for Game {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "{:?} {:?}", self.hand.cards, self.bid)
-    }
-}
-
 fn is_valid_hand(c: char) -> bool {
     "AQKJT98765432".contains(c)
+}
+
+fn parse_card(input: &str) -> IResult<&str, Card> {
+    map_res(take_while_m_n(1, 1, is_valid_hand), |x: &str| {
+        x.chars().next().unwrap().try_into()
+    })(input)
 }
 
 fn parse_hand(input: &str) -> IResult<&str, Hand> {
@@ -188,16 +172,16 @@ fn parse_bid(input: &str) -> IResult<&str, i32> {
 }
 
 fn parse_line(input: &str) -> IResult<&str, Game> {
-    map(tuple((parse_hand, tag(" "), parse_bid)), |(x, _, z)| Game {
+    map(tuple((parse_hand, tag(" "), parse_bid)), |(x, y, z)| Game {
         hand: x,
         bid: z,
     })(input)
 }
 
 fn main() {
-    let data = include_str!("../data/data.txt").lines();
+    let data = include_str!("../../data/data.txt").lines();
 
-    let games = data
+    let mut games = data
         .map_while(|line| {
             all_consuming(parse_line)(line)
                 .finish()
@@ -209,11 +193,54 @@ fn main() {
     let a = games
         .iter()
         .sorted_by(|a, b| a.hand.partial_cmp(&b.hand).unwrap())
+        .rev()
         .enumerate()
-        .inspect(|(_, game)| {
-            println!("{}", game);
-        })
         .map(|(index, game)| (index + 1) * game.bid as usize)
         .sum::<usize>();
     dbg!(a);
+}
+
+#[test]
+fn test_parse_card() {
+    assert_eq!(parse_card("A"), Ok(("", Card::A)));
+}
+
+#[test]
+fn test_parse_hand() {
+    assert_eq!(
+        parse_hand("AAA22"),
+        Ok((
+            "",
+            Hand {
+                cards: vec![Card::A, Card::A, Card::A, Card::H2, Card::H2],
+            }
+        ))
+    );
+}
+
+#[test]
+fn test() {
+    let cards = vec![Card::A, Card::A, Card::A, Card::A, Card::A];
+    let a: HandKind = cards.try_into().unwrap();
+    assert_eq!(a, HandKind::FIVE_KIND);
+
+    let cards = vec![Card::A, Card::A, Card::A, Card::A, Card::Q];
+    let a: HandKind = cards.try_into().unwrap();
+    assert_eq!(a, HandKind::FOUR_KIND);
+
+    let cards = vec![Card::A, Card::A, Card::A, Card::Q, Card::Q];
+    let a: HandKind = cards.try_into().unwrap();
+    assert_eq!(a, HandKind::FULL_HOUSE);
+
+    let cards = vec![Card::A, Card::A, Card::Q, Card::A, Card::Q];
+    let a: HandKind = cards.try_into().unwrap();
+    assert_eq!(a, HandKind::FULL_HOUSE);
+
+    let cards = vec![Card::A, Card::A, Card::A, Card::T, Card::Q];
+    let a: HandKind = cards.try_into().unwrap();
+    assert_eq!(a, HandKind::THREE_OF_KIND);
+
+    let cards = vec![Card::T, Card::A, Card::A, Card::A, Card::Q];
+    let a: HandKind = cards.try_into().unwrap();
+    assert_eq!(a, HandKind::THREE_OF_KIND);
 }
